8c0ec8556d70ae8994563e12a81cc116a5bdb3a1
Edmorapez <edmorapez@gmail.com>
2016-10-20 23:43:12 -0500

    Escane_l

---
+++ src/escanearprocesos/ClienteBroadcast.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escanearprocesos;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Edgar1
+ */
+public class ClienteBroadcast {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         try {
+            ClienteBroadcast instancia = new ClienteBroadcast();
+            instancia.iniciarConexion();
+            instancia.inicia();
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteBroadcast.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void inicia(){
+        while(true){
+            try {
+                int puerto = 1235;
+                ServerSocket socketPrincipal = new ServerSocket(puerto);
+                System.out.println("Esperando escaner conexion del servidor...");
+
+                Socket socketEntrante = socketPrincipal.accept();
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(socketEntrante.getInputStream()));
+                PrintWriter out = new PrintWriter(socketEntrante.getOutputStream(), true);
+                
+                String entrada = "";
+                String comandoSalir = "Exit";
+
+                while ((entrada = in.readLine()) != null) {
+                  
+                    switch(entrada){
+                           
+                        case "EscanearPuertos":
+                            
+                            out.println(obtenerProcesos());
+                            break;
+                                
+                        default:
+                            if (entrada.trim().equals(comandoSalir)){
+                                return;
+                            }
+                            break;
+                    }
+                                  
+                    
+                }
+                socketEntrante.close();
+                socketPrincipal.close();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }           
+    }
+    
+    public List<String> obtenerProcesos(){
+        System.out.println("ObtenerPRocesos");
+        List<String> proceso = new ArrayList<String>();
+        
+        EscanearProcesos escanearProcesos = new EscanearProcesos();
+        proceso = escanearProcesos.listRunningProcesses();
+        String result = "";
+        // display the result 
+        Iterator<String> it = proceso.iterator();
+        int i = 0;
+        while (it.hasNext()) {
+            result += it.next() +"\n,";
+            i++;
+            if (i==10) {
+                result += "\n";
+                i = 0;
+            }
+        }
+        //System.out.print(result);
+        return proceso;
+    }
+    
+    private String obtenerNombreMaquina(){
+       
+        InetAddress address=null;
+        try {
+            address = InetAddress.getLocalHost();
+        } catch (Throwable ex) {
+            //Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        }
+        //System.out.println("Nombre de la Maquina: " + address);
+        return "" + address;
+    }
+    
+    private void iniciarConexion() throws IOException{
+        System.out.println("Esperando conexion Servidor...");
+        int puerto=1234;
+        
+       // String ip="192.168.0.12";
+         String ip="127.0.0.1";
+        Socket cliente = null;
+        outer:
+        while(true){
+            try{       
+                
+                cliente = new Socket(ip,puerto);
+                BufferedReader entCliente=new BufferedReader(new InputStreamReader(cliente.getInputStream()));              
+                String cadena= entCliente.readLine();
+                System.out.println("Entrada: "+cadena);
+                
+                System.out.println("entra cadena");
+
+                PrintWriter salCliente = new PrintWriter(cliente.getOutputStream()); 
+                salCliente.println(obtenerNombreMaquina());            
+                salCliente.close();
+                
+                entCliente.close();
+                cliente.close();
+                System.out.println("ANtes del outer");
+                break outer;
+             }
+             catch(Exception e){
+                 System.out.println(e.getMessage());
+             }
+        }
+        
+    }
+}
---
+++ src/escanearprocesos/EscanearProcesos.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escanearprocesos;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+/**
+ *
+ * @author Edgar1
+ */
+public class EscanearProcesos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Corriendo");
+        List<String> processes = listRunningProcesses();
+        String result = "";
+        // display the result 
+        Iterator<String> it = processes.iterator();
+        int i = 0;
+        while (it.hasNext()) {
+            result += it.next() +"\n,";
+            i++;
+            if (i==10) {
+                result += "\n";
+                i = 0;
+            }
+        }
+        msgBox("Running processes : " + result);
+        System.out.println(result);
+    }
+     public static List<String> listRunningProcesses() {
+        List<String> processes = new ArrayList<String>();
+        try {
+            String line;
+            //Process p = Runtime.getRuntime().exec("tasklist.exe /fo csv /nh");
+            //System.getenv("windir") +"\\system32\\"+"tasklist.exe"
+            Process p = Runtime.getRuntime().exec(System.getenv("windir") +"\\system32\\"+"tasklist.exe");
+            BufferedReader input = new BufferedReader
+                (new InputStreamReader(p.getInputStream()));
+                while ((line = input.readLine()) != null) {
+                    if (!line.trim().equals("")) {
+                        // keep only the process name
+                        line = line.substring(1);
+                        //processes.add(line.substring(0, line.indexOf("")));
+                        processes.add(line);
+                        
+                    }
+                }
+            input.close();
+        }
+        catch (Exception err) {
+          err.printStackTrace();
+        }
+        return processes;
+    }
+    public static void msgBox(String msg) {
+    javax.swing.JOptionPane.showConfirmDialog((java.awt.Component)
+       null, msg, "WindowsUtils", javax.swing.JOptionPane.DEFAULT_OPTION);
+    } 
+}
